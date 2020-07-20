893bb641253df35f41ccadfd574d26b4ec302a48
isabelcooper <isabel@triptease.com>
2020-07-20 20:09:25 +0100

    add simple interaction with console (repeat back given string)

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hi');
+(async () => {
+    process.stdin.on('data', function (chunk) {
+        console.log('received: ', chunk.toString());
+        if (chunk.toString() === 'q') process.exit();
+    });
+})();
